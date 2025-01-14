687959e8d3c6316a012bfbf0a7573f4b237c1d65
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2025-01-14 20:12:02 +0600

    added

--- src/routes/admin.route.tsx
+++ src/routes/admin.route.tsx
@@ -1,20 +1,10 @@
-import { ReactNode } from "react";
+
 import AdminDashboard from "../pages/Admin/AdminDashboard";
 import CreateAdmin from "../pages/Admin/CreateAdmin";
 import CreateFaculty from "../pages/Admin/CreateFaculty";
 import CreateStudent from "../pages/Admin/CreateStudent";
-import { NavLink } from "react-router-dom";
 
 
-// type TRoute = {
-//     path: string;
-//     element: ReactNode;
-// }
-type TSidebarItem = {
-    key: string;
-    label: ReactNode;
-    children?: TSidebarItem[];
-}
 
  export const adminPaths = [
     
@@ -52,27 +42,27 @@ type TSidebarItem = {
 ]
 
 
-export const adminSidebarItems = adminPaths.reduce((acc:TSidebarItem[],item) => {
-    if(item.path && item.name){
-        acc.push({
-            key: item.name,
-            label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
-        });
-    }
+// export const adminSidebarItems = adminPaths.reduce((acc:TSidebarItem[],item) => {
+//     if(item.path && item.name){
+//         acc.push({
+//             key: item.name,
+//             label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+//         });
+//     }
 
-    if(item.children){
-        acc.push({
-            key: item.name,
-            label:item.name,
-            children: item.children.map((child) => ({
-                key: child.name,
-                label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
-            }))
+//     if(item.children){
+//         acc.push({
+//             key: item.name,
+//             label:item.name,
+//             children: item.children.map((child) => ({
+//                 key: child.name,
+//                 label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+//             }))
 
-            });
-    }
-    return acc;
-}, [])
+//             });
+//     }
+//     return acc;
+// }, [])
 
 
 //* Programatical way
